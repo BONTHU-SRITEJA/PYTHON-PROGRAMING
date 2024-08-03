78451255cda9089aa8a6bf16760f483b9209150f
Sriteja bonthu <23pa1a0535@vishnu.edu.in>
2024-08-03 10:53:15 +0530

    Sum of first N natural Numbers using loops

---
+++ sum.py
@@ -0,0 +1,7 @@
+# to find the sum of N natural numbers using loops
+num=int(input("ENTER THE NUMBER :"))
+sum=0
+
+for i in range(1,num+1) :
+    sum+=i
+print("Sum of",num,"Natural Numbers is :",sum)	
