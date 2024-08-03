b3245a4fe7e7278f8e4d0f163238f63b00c5c2a2
Sriteja bonthu <23pa1a0535@vishnu.edu.in>
2024-08-03 11:14:30 +0530

    To print each character of a string and its length

---
+++ string.py
@@ -0,0 +1,8 @@
+# To print each character of a string and string length
+mystr=input("ENTER A STRING :")
+count=0
+for x in mystr :
+    print(x)
+    count+=1
+# to print the length of the string
+print("The length of the String:",count)
